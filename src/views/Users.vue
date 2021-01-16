3bcae35ce4eb384a34f1540789a566b353e2b90a
AntoinePereira <toine_42@hotmail.fr>
2021-01-16 18:42:06 +0100

    Initialisation, Vuex, router

---
+++ src/views/Users.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="users">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App Users"/>
+  </div>
+</template>
+
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Users',
+  components: {
+    HelloWorld
+  }
+}
+</script>
